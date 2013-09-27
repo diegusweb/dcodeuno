ff74e3abe61e3d67816b32add48aff6ab682e5b9
Marcos Troncoso <mtroncoso@prosoftnearshore.com>
2013-09-27 18:55:21 -0400

    nuaaaaaaaaaaa

--- actor.php
+++ actor.php
@@ -2,7 +2,7 @@
 
 require 'functions.php';
 connect();
-//BRACNH LOCAL TH BRACNH LOCAL TH dddddddddd  OTRO
+//BRACNH LOCAL TH BRACNH LOCAL TH dddddddddd  sdf fsd fsdf sd OTRO
 $info = get_actor_info( $_GET['actor_id'] );
 
 include 'views/actor.tmpl.php';
